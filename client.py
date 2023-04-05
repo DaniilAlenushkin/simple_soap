b9b94fb23efbbbbc6a1e6e790522436c95a96437
Daniil Alenushkin <daniilalenushkin@yandex.ru>
2023-04-06 00:23:32 +0300

    realized simple soap

---
+++ client.py
@@ -0,0 +1,65 @@
+from zeep import Client
+
+
+def get_documentation(wsdl):
+    import operator
+    for service in wsdl.services.values():
+        print(str(service))
+        for port in service.ports.values():
+            print(" " * 4, str(port))
+            print(" " * 8, "Operations:")
+            operations = sorted(port.binding._operations.values(), key=operator.attrgetter("name"))
+            for operation in operations:
+                print("%s%s" % (" " * 12, str(operation)))
+            print("")
+
+
+if __name__ == '__main__':
+    url = 'http://localhost:8000/event/soap?wsdl'
+    client = Client(url)
+    # client.wsdl.dump()
+    get_documentation(client.wsdl)
+
+    print('Получение всех событий')
+    events = client.service.read_all_events()
+    for event in events:
+        print(event.id, event.name, event.participant_number)
+
+    print('\nСоздание события')
+    new_event = client.service.create_event('НОВОЕ СОБЫТИЕ', 99)
+    print('Создано событие:', new_event.id, new_event.name, new_event.participant_number)
+
+    print('\nПолучение всех событий')
+    events = client.service.read_all_events()
+    for event in events:
+        print(event.id, event.name, event.participant_number)
+
+    print('\nПолучение некоторых событий событий')
+    events = client.service.read_all_events(name='в')
+    for event in events:
+        print(event.id, event.name, event.participant_number)
+
+    print('\nПолучение события по ID')
+    event_id = new_event.id
+    event = client.service.read_event(event_id)
+    print('Событие:', event_id, event.name, event.participant_number)
+
+    print('\nОбновление события по ID')
+    event_id = new_event.id
+    update_event = client.service.update_event(event_id, name='ОБНОВЛЕННОЕ СОБЫТИЕ', participant_number=12)
+    print('Обновлено событие:', update_event.id, update_event.name, update_event.participant_number)
+
+    print('\nПолучение всех событий')
+    events = client.service.read_all_events()
+    for event in events:
+        print(event.id, event.name, event.participant_number)
+
+    print('\nУдаление события по ID')
+    event_id = new_event.id
+    delete_id = client.service.delete_event(event_id)
+    print('Удалено событие с ID', delete_id)
+
+    print('\nПолучение всех событий')
+    events = client.service.read_all_events()
+    for event in events:
+        print(event.id, event.name, event.participant_number)
---
+++ server.py
@@ -0,0 +1,94 @@
+from spyne import Application, rpc, ServiceBase, Integer, Unicode, Array, ComplexModel
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
+
+
+# Модель Event
+class Event(ComplexModel):
+    id = Integer
+    name = Unicode
+    participant_number = Integer
+
+    def __init__(self, id, name, participant_number):
+        self.id = id
+        self.name = name
+        self.participant_number = participant_number
+
+
+# База данных
+events = [Event(1, 'Летний IT-лагерь в Компьютерной Академии', 9),
+          Event(2, 'VR Рисование в Виртуальной реальности', 6),
+          Event(3, 'Невероятное путешествие по Музею Эмоций', 18),
+          Event(4, 'Майские праздники в Музее автомобильных историй', 17),
+          Event(5, 'Шоу «Big Stand Up»', 20),
+          Event(6, 'Субботник "Под крышей дома моего"', 14),
+          Event(7, 'Волонтерский проект "Delevery-бабушка"', 9),
+          Event(8, 'Вечеринка "Этонекоронабро"', 16),
+          Event(9, 'Большая летняя Спартакиада в Лужниках', 6),
+          Event(10, 'Международный день Солнца в Дарвиновском музее', 9)]
+
+
+# Сервис Event
+class EventService(ServiceBase):
+    # Создание события
+    @rpc(Unicode, Integer, _returns=Event)
+    def create_event(ctx, name, participant_number):
+        new_event_id = max([event.id for event in events]) + 1
+        new_event = Event(new_event_id, name, participant_number)
+        events.append(new_event)
+        return new_event
+
+    # Получение события по ID
+    @rpc(Integer, _returns=Event)
+    def read_event(ctx, id):
+        for event in events:
+            if event.id == id:
+                return event
+        raise ValueError('Event not found')
+
+    # Получение всех событий
+    @rpc(Unicode, Integer, _returns=Array(Event))
+    def read_all_events(ctx, name=None, participant_number=None):
+        if name or participant_number is not None:
+            filtered_events = []
+            for event in events:
+                if name and name in event.name:
+                    filtered_events.append(event)
+                elif participant_number is not None and  participant_number < event.participant_number:
+                    filtered_events.append(event)
+            return filtered_events
+        return events
+
+    # Обновление события по ID
+    @rpc(Integer, Unicode, Integer, _returns=Event)
+    def update_event(ctx, id, name=None, participant_number=None):
+        for event in events:
+            if event.id == id:
+                if name:
+                    event.name = name
+                if participant_number:
+                    event.participant_number = participant_number
+                return event
+        raise ValueError('Event not found')
+
+    # Удаление события по ID
+    @rpc(Integer, _returns=Integer)
+    def delete_event(ctx, id):
+        for i, event in enumerate(events):
+            if event.id == id:
+                del events[i]
+                return id
+        raise ValueError('Event not found')
+
+
+# Создание приложения и добавление сервиса Event
+application = Application([EventService], 'http://example.com/event/soap',
+                          in_protocol=Soap11(validator='lxml'),
+                          out_protocol=Soap11())
+
+# Запуск сервера
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    server = make_server('localhost', 8000, WsgiApplication(application))
+    server.serve_forever()
